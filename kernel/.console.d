0b94ef6d9c58e94a5010c3c60dad936922c81dce
lufeee <exiga.mad@gmail.com>
2021-08-05 12:30:26 +0000

    delete .d file

--- kernel/.console.d
+++
@@ -1,6 +0,0 @@
-console.o: console.cpp console.hpp graphics.hpp frame_buffer_config.hpp \
-  /home/vagrant/osbook/devenv/x86_64-elf/include/c++/v1/stdint.h \
-  /home/vagrant/osbook/devenv/x86_64-elf/include/c++/v1/__config \
-  /home/vagrant/osbook/devenv/x86_64-elf/include/c++/v1/cstring \
-  /home/vagrant/osbook/devenv/x86_64-elf/include/c++/v1/string.h \
-  font.hpp /home/vagrant/osbook/devenv/x86_64-elf/include/c++/v1/cstdint
--- kernel/.main.d
+++
@@ -1,9 +0,0 @@
-main.o: main.cpp \
-  /home/vagrant/osbook/devenv/x86_64-elf/include/c++/v1/cstdint \
-  /home/vagrant/osbook/devenv/x86_64-elf/include/c++/v1/__config \
-  /home/vagrant/osbook/devenv/x86_64-elf/include/c++/v1/stdint.h \
-  /home/vagrant/osbook/devenv/x86_64-elf/include/c++/v1/cstddef \
-  /home/vagrant/osbook/devenv/x86_64-elf/include/c++/v1/version \
-  /home/vagrant/osbook/devenv/x86_64-elf/include/c++/v1/cstdio \
-  /home/vagrant/osbook/devenv/x86_64-elf/include/c++/v1/stdio.h \
-  frame_buffer_config.hpp graphics.hpp font.hpp console.hpp
